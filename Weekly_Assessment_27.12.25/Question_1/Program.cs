dac5d739ff889a1a7ea20f99a0c4fb47c014449b
Arti Thakur <arti31thakur@gmail.com>
2025-12-27 16:27:39 +0530

    Code

--- Weekly_Assessment_27.12.25/Question_1/Program.cs
+++ Weekly_Assessment_27.12.25/Question_1/Program.cs
@@ -1,2 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("============= QuickMart Traders ==============");
+            Console.WriteLine("1. Create New Transaction (Enter Purchase & Selling Details)");
+            Console.WriteLine("2. View Last Transaction");
+            Console.WriteLine("3. Calculate Profit/Loss (Recompute & Print)");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your option: ");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                SaleTransaction.CreateMethod();
+                break;
+
+                case 2:
+                SaleTransaction.ViewMethod();
+                break;
+
+                case 3:
+                SaleTransaction.ClearMethod();
+                break;
+
+                case 4:
+                Console.WriteLine("Thank you. Application closed normally.");
+                exit = true;
+                break;
+
+                default:
+                Console.WriteLine("Invalid option!");
+                break;
+
+            }
+        }
+    }
+}
--- Weekly_Assessment_27.12.25/Question_1/QuickMart.cs
+++ Weekly_Assessment_27.12.25/Question_1/QuickMart.cs
@@ -1,23 +1,121 @@
 using System;
 
-public class SaleTransaction
+class SaleTransaction
 {
-    public static void SaleMain()
+    static string InvoiceNo;
+    static string CustomerName;
+    static string ItemName;
+    static int Quantity;
+    static double PurchaseAmount;
+    static double SellingAmount;
+    static string ProfitOrLossStatus;
+    static double ProfitOrLossAmount;
+    static double ProfitMarginPercent;
+
+    public static void CreateMethod()
     {
-        Console.WriteLine("================== QuickMart Traders =================");
+        Console.Write("Enter Invoice No: ");
+        InvoiceNo = Console.ReadLine();
 
-        Console.WriteLine("Enter Invoice No: ");
-        string InvoiceNo = console.ReadLine();
+        if(InvoiceNo == null)
+        {
+            return;
+        }
 
-        Console.WriteLine("Enter Customer Name: ");
-        string CustomerName = console.ReadLine();
+        Console.Write("Enter Customer Name: ");
+        CustomerName = Console.ReadLine();
 
-        Console.WriteLine("Enter Item Name: ");
-        string ItemName = console.ReadLine();
+        Console.Write("Enter Item Name: ");
+        ItemName = Console.ReadLine();
 
-        Console.WriteLine("Enter Quantity: ");
-        int Quantity = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Quantity: ");
+        Quantity = Convert.ToInt32(Console.ReadLine());
 
-        
+        if(Quantity <= 0)
+        {
+            Console.Write("Quantity must be greater than 0");
+            return;
+        }
+
+        Console.Write("Enter total Purchase Amount: ");
+        PurchaseAmount = Convert.ToDouble(Console.ReadLine());
+
+        if(PurchaseAmount <= 0)
+        {
+            Console.Write("Purchase amount must be greater than 0");
+            return;
+        }
+
+        Console.Write("Enter total Selling Amount: ");
+        SellingAmount = Convert.ToDouble(Console.ReadLine());
+
+        if(SellingAmount < 0)
+        {
+            Console.Write("Selling Amount must be greater than 0");
+            return;
+        }
+
+        if(SellingAmount > PurchaseAmount)
+        {
+            ProfitOrLossStatus = "PROFIT";
+            ProfitOrLossAmount = SellingAmount - PurchaseAmount;
+        }
+
+        else if(SellingAmount < PurchaseAmount)
+        {
+            ProfitOrLossStatus = "LOSS";
+            ProfitOrLossAmount = PurchaseAmount - SellingAmount;
+        }
+
+        else if(SellingAmount == PurchaseAmount)
+        {
+            ProfitOrLossStatus = "BREAK-EVEN";
+            ProfitOrLossAmount = 0;
+        }
+
+        double percent = (ProfitOrLossAmount / PurchaseAmount) * 100;
+        ProfitMarginPercent = Math.Round(percent, 2);
+
+        Console.WriteLine("Transaction saved successfully.");
+        Console.WriteLine("Status: " + ProfitOrLossStatus);
+        Console.WriteLine("Profit/Loss Amount: " + ProfitOrLossAmount);
+        Console.WriteLine("Profit Margin (%): " + ProfitMarginPercent);
+    }
+     // ----------------------- 2nd method ----------------
+    public static void ViewMethod()
+    {
+        if (InvoiceNo == null)
+        {
+            Console.WriteLine("No transaction available. Please create a new transaction first.");
+            return;
+        }
+
+        Console.WriteLine("----------- Last Transaction -----------");
+        Console.WriteLine("InvoiceNo: " + InvoiceNo);
+        Console.WriteLine("Customer: " + CustomerName);
+        Console.WriteLine("Item: " + ItemName);
+        Console.WriteLine("Quantity: " + Quantity);
+        Console.WriteLine("Purchase Amount: " + PurchaseAmount);
+        Console.WriteLine("Selling Amount: " + SellingAmount);
+        Console.WriteLine("Status: " + ProfitOrLossStatus);
+        Console.WriteLine("Profit/Loss Amount: " + ProfitOrLossAmount);
+        Console.WriteLine("Profit Margin(%): " + ProfitMarginPercent);
+        Console.WriteLine("--------------------------------");
+    }
+
+    // -------------------- 3rd method ------------------------------
+
+    public static void ClearMethod()
+    {
+        InvoiceNo = null;
+        CustomerName = null;
+        ItemName = null;
+        Quantity = 0;
+        PurchaseAmount = 0;
+        SellingAmount = 0;
+        ProfitOrLossStatus = null;
+        ProfitOrLossAmount = 0;
+        ProfitMarginPercent = 0;
+
     }
 }
