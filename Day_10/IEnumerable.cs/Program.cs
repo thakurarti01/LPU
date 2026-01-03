0aff95a3a6f492b1409e0968f318115ddeba9006
Arti Thakur <arti31thakur@gmail.com>
2026-01-03 23:59:45 +0530

    Code

---
+++ Day_10/IEnumerable.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // create players
+        Player player = new Player("Alex");
+
+        // add skills
+        player.AddSkill(new Skill("Shooting", 8));
+        player.AddSkill(new Skill("Passing", 7));
+        player.AddSkill(new Skill("Dribbling", 9));
+        player.AddSkill(new Skill("Defending", 6));
+
+        // iterate over Player directly (Player behaves as a skill collection)
+
+        Console.WriteLine($"Player: {player.PlayerName}");
+        Console.WriteLine("Skills: ");
+
+        foreach(Skill skill in player)
+        {
+            Console.WriteLine(skill);
+        }
+
+    }
+}
---
+++ Day_10/IEnumerable.cs/Skill.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Skill
+{
+    public string SkillName {get; set;}
+    public int Level {get; set;}
+
+    public Skill(string skillName, int level)
+    {
+        SkillName = skillName;
+        Level = level;
+    }
+
+    public override string ToString()
+    {
+        return $"Skill: {SkillName}, Level: {Level}";
+    }
+}
+
+//player class behaves like a collection of skill
+public class Player : IEnumerable<Skill>
+{
+    public string PlayerName{get; set;}
+
+    //skill collection property
+    public List <Skill> Skills {get; set;}
+
+    public Player(string playerName)
+    {
+        PlayerName = playerName;
+        Skills = new List <Skill>();
+    }
+
+    //add skill to player
+    public void AddSkill(Skill skill)
+    {
+        Skills.Add(skill);
+    }
+
+    //IEnumerable implementation
+    public IEnumerable<Skill> GetEnumerator()
+    {
+        return Skills.GetEnumerator();
+    }
+
+    //non generic IEnumerator implementation
+    IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+}
