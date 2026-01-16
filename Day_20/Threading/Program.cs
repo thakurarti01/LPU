fcc1d8335672e0bc5fa7551c41cd90fc8bb301b0
Arti Thakur <arti31thakur@gmail.com>
2026-01-16 10:24:59 +0530

    Threading

---
+++ Day_20/Threading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        ThreadStart task1 = new ThreadStart(ProcessTask1);
+        ThreadStart task2 = new ThreadStart(ProcessTask2);
+
+        Thread t1 = new Thread(task1);
+        Thread t2 = new Thread(task2);
+
+        t1.Start();
+        t2.Start();
+
+        Console.ReadLine();
+    }
+
+    static void ProcessTask1()
+    {
+        for(int i = 0; i<5; i++)
+        {
+            Console.WriteLine("Thread t1 in action");
+            Thread.Sleep(5);
+        }
+    }
+
+    static void ProcessTask2()
+    {
+        for(int i = 0; i<5; i++)
+        {
+            Console.WriteLine("Thread t2 in action");
+            Thread.Sleep(10);
+        }
+    }
+}
