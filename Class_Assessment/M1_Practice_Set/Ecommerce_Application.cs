1cbd1711a3d45a0da43259cc40e12d0809d64b1c
Arti Thakur <arti31thakur@gmail.com>
2026-01-17 15:30:34 +0530

    M1 Practice Set

---
+++ Class_Assessment/M1_Practice_Set/Ecommerce_Application.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class EcommerceShop
+{
+    public string UserName {get; set;}
+    public double WalletBalance {get; set;}
+    public double TotalPurchaseAmount {get; set;}
+}
+
---
+++ Class_Assessment/M1_Practice_Set/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class InsufficientWalletBalanceException : Exception
+{
+    public InsufficientWalletBalanceException(string message) : base (message)
+    {}
+}
+class Program
+{
+    public EcommerceShop MakePayment(string name, double balance, double amount)
+    {
+        EcommerceShop ecomObj = new EcommerceShop();
+        ecomObj.UserName = name;
+        ecomObj.WalletBalance = balance;
+        ecomObj.TotalPurchaseAmount = amount;
+
+        if(amount>balance)
+        {
+            throw new InsufficientWalletBalanceException("Insufficient balance in your digital wallet");
+        }
+        Console.WriteLine("Order received!");
+        return ecomObj;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter your name");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter Balance");
+        double balance = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter Amount:");
+        double amount = Convert.ToDouble(Console.ReadLine());
+
+        Program program = new Program();
+
+        try
+        {
+            program.MakePayment(name, balance, amount);
+        }
+        catch(InsufficientWalletBalanceException ex)
+        {
+                Console.WriteLine(ex.Message);
+        }
+        
+    }
+}
+
