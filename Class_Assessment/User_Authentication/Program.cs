a1009531d43792f1a4f4bc29b312f933d908889c
Arti Thakur <arti31thakur@gmail.com>
2026-01-17 15:50:24 +0530

    M1 Practice Set Question

---
+++ Class_Assessment/User_Authentication/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class PasswordMismatchException : Exception
+{
+    public PasswordMismatchException(string message) : base(message)
+    {}
+}
+class Program
+{
+    public User ValidatePassword(string name, string password, string confirmationPassword)
+    {
+        User userObj = new User();
+
+        userObj.Name = name;
+        userObj.Password = password;
+        userObj.ConfirmationPassword = confirmationPassword;
+
+        if(password != confirmationPassword)
+        {
+           throw new PasswordMismatchException("Password entered does not match"); 
+        }
+        Console.WriteLine("Registered Successfully");
+        return userObj;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter password:");
+        string password = Console.ReadLine();
+        Console.WriteLine("Enter your password again:");
+        string confirmationPassword = Console.ReadLine();
+
+        Program program = new Program();
+
+        try
+        {
+            program.ValidatePassword(name, password, confirmationPassword);
+        }
+        catch(PasswordMismatchException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ Class_Assessment/User_Authentication/User.cs
@@ -0,0 +1,8 @@
+using System;
+
+class User
+{
+    public string Name {get; set;}
+    public string Password {get; set;}
+    public string ConfirmationPassword {get; set;}
+}
