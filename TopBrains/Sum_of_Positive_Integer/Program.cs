0b2f4ee9ea9991e120fb60425884676e082020a6
Arti Thakur <arti31thakur@gmail.com>
2026-01-20 00:27:28 +0530

    TopBrains Questions

--- TopBrains/Sum_of_Positive_Integer/Program.cs
+++ TopBrains/Sum_of_Positive_Integer/Program.cs
@@ -8,6 +8,29 @@ class Program
         int size = Convert.ToInt32(Console.ReadLine());
 
         int[] arr = new int[size];
+        Console.WriteLine("Enter elements of array: ");
+        for(int i = 0; i<size; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int sum = 0;
+        for(int i = 0; i<size; i++)
+        {
+            // sum += arr[i];
+            if(arr[i] == 0)
+            {
+                break;
+            }
+            else if(arr[i] < 0)
+            {
+                continue;
+            }
+            else
+            {
+                sum += arr[i];
+            }
+        }
+        Console.WriteLine("Sum: " + sum); 
     }
 
 
