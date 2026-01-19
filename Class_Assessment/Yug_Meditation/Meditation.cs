6a850f0e73f7079b07295d76499f60160b7ee955
Arti Thakur <arti31thakur@gmail.com>
2026-01-19 23:04:04 +0530

    M1 Practice Set Questions

---
+++ Class_Assessment/Yug_Meditation/Meditation.cs
@@ -0,0 +1,11 @@
+using System;
+
+class MeditationCenter
+{
+    public int MemberId {get; set;}
+    public int Age {get; set;}
+    public double Weight {get; set;}
+    public double Height {get; set;}
+    public string Goal {get; set;}
+    public double BMI {get; set;}
+}
---
+++ Class_Assessment/Yug_Meditation/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    public static ArrayList memberList = new ArrayList();
+
+    public void AddYogaMember(int memberId, int age, double weight, string goal)
+    {
+        MeditationCenter member = new MeditationCenter();
+        member.MemberId = memberId;
+        member.Age = age;
+        member.Weight = weight;
+        member.Goal = goal;
+
+        memberList.Add(member);
+    }
+
+    public double CalculateBMI(int memberId)
+    {
+        foreach(MeditationCenter member in memberList)
+        {
+            if(member.MemberId == memberId)
+            {
+                Console.WriteLine("Enter height in meter: ");
+                double height = double.Parse(Console.ReadLine());
+
+                double bmi = member.Weight / (height * height);
+                member.BMI = bmi;
+                return bmi;
+            }
+        }
+        return 0;
+    }
+
+    public int CalculateYogaFee(int memberId)
+    {
+        foreach(MeditationCenter member in memberList)
+        {
+            if(member.MemberId == memberId)
+            {
+                if(member.Goal == "WeightLoss")
+                {
+                    return 3000;
+                }
+                else if(member.Goal == "Flexibility")
+                {
+                    return 2500;
+                }
+                else
+                {
+                    return 2000;
+                }
+            }
+        }
+        return 0;
+    }
+    static void Main()
+    {
+        Program p = new Program();
+
+        p.AddYogaMember(1, 25, 70, "WeightLoss");
+        Console.WriteLine("BMI: " + p.CalculateBMI(1));
+        Console.WriteLine("Fee: " + p.CalculateYogaFee(1));
+    }
+
+}
