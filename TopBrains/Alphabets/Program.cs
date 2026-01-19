fbb84b65cd427d222fd7bf42f13c4c8154d506e4
Arti Thakur <arti31thakur@gmail.com>
2026-01-19 12:00:44 +0530

    TopBrains Questions

--- TopBrains/Alphabets/Program.cs
+++ TopBrains/Alphabets/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first word: ");
+        string first = Console.ReadLine();
+
+        Console.WriteLine("Enter second word: ");
+        string sec = Console.ReadLine();
+
+        string res = ""'
+
+        // --- REMOVING COMMON CONSTANTS
+        char[] first1 = first.ToCharArray();
+        char[] sec1 = sec.ToCharArray();
+
+        for(int i = 0; i<first1.Length; i++)
+        {
+            for(int j = 0; j<sec1.Length; j++)
+            {
+                if(first1[i] == sec1[j])
+                {
+                    first1.Remove();
+                }
+            }
+        }
+    }
+}
