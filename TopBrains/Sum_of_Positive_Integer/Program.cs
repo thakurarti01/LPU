965c8eb6be193d20a2bbcd8883627148e04fad3b
Arti Thakur <arti31thakur@gmail.com>
2026-01-19 21:43:26 +0530

    TopBrains Questions

---
+++ TopBrains/Sum_of_Positive_Integer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the size of the array: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[size];
+    }
+
+
+    
+}
