08e834f362f9e5960c597cb3192271c1d49bf22f
Arti Thakur <arti31thakur@gmail.com>
2026-01-28 14:38:00 +0530

    Weekly Assessment

---
+++ Weekly_Assessment_24.01.26/Vehicle_Driving_Simulation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Vehicle
+{
+    //your code goes here
+    protected int wheels;
+    public Vehicle(int wheels)
+    {
+        this.wheels = wheels;
+    }
+    public virtual string Drive()
+    {
+        return $"{wheels} wheeler driving";
+    }    
+}
+
+class TwoWheeler : Vehicle
+{
+   //your code goes here
+   public TwoWheeler() : base(2){}
+
+}
+
+class HMV : Vehicle
+{
+    //your code goes here
+    public HMV(int wheels) : base(wheels){}
+}
+
+class Source
+{
+    static void Main(string[] args)
+    {
+       //your code goes here
+       Vehicle obj1 = new TwoWheeler();
+       Vehicle obj2 = new HMV(8);
+
+       Console.WriteLine(obj1.Drive());
+       Console.WriteLine(obj2.Drive());
+    }
+}
