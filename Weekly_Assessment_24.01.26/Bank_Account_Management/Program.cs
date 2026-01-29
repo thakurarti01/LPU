a8521aa8604e69888b8e7c490c0842355129246c
Arti Thakur <arti31thakur@gmail.com>
2026-01-29 11:48:51 +0530

    Weekly Assessment

--- Weekly_Assessment_24.01.26/Bank_Account_Management/Program.cs
+++ Weekly_Assessment_24.01.26/Bank_Account_Management/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace BankSys
+{
+    public class Account
+    {
+        // TODO: Add private fields
+        private string name;
+        private double balance;
+        
+        // TODO: Implement constructor
+        public Account(string name, double initialBalance)
+        {
+            this.name = name;
+            this.balance = initialBalance;
+        }
+        
+        // TODO: Implement deposit method
+        public double deposit(double amount)
+        {
+            balance += amount;
+            return balance;
+        }
+        
+        // TODO: Implement getBalance method
+        public double getBalance()
+        {
+            return balance;
+        } 
+        
+        // TODO: Implement setName method
+        public void setName(string newName)
+        {
+            name = newName;
+        }
+        
+        // TODO: Implement getName method
+        public string getName()
+        {
+            return name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test your implementation here
+            Account account1 = new Account("Alok Mittal", 1250.00);
+            Console.WriteLine(account1.getBalance());
+            Account account2 = new Account("John Doe", 500);
+            Console.WriteLine(account2.getName());
+            Console.WriteLine(account2.getBalance());
+            Account account3 = new Account("Riya Amit Mehta ", 1250);
+            Console.WriteLine(account3.deposit(0.5));
+            Console.WriteLine(account3.getBalance());
+
+            Console.WriteLine(account3.getName());
+        }
+    }
+}
