5e450536d81a8e4d6d055802ad775b187c3e4d00
Arti Thakur <arti31thakur@gmail.com>
2026-01-31 09:58:24 +0530

    M1_Collections

---
+++ M1_Collections/Bake_Me_A_Wish/Bake.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+public class CakeOrder
+{
+    private Dictionary<string, double> orderMap;
+
+    public void addOrderDetails(string orderId, double cakeCost)
+    {
+        
+    }
+}
