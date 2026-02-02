0219040353a84c7d987f886255c1efbf2597876b
Arti Thakur <arti31thakur@gmail.com>
2026-02-02 21:42:18 +0530

    M1_Strings

---
+++ M1_Strings/Password_Generation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the username");
+        string user = Console.ReadLine();
+
+        bool validUsername = user.Length == 8 && 
+                             Regex.IsMatch(user.Substring(0, 4), @"^[A-Z]{4}$")&&
+                             user[4] == '@' &&
+                             int.TryParse(user.Substring(5, 3), out int num) && (num>=101 && num<=115);
+
+        if (!validUsername)
+        {
+            Console.WriteLine(user + " is an invalid username");
+            return;
+        }
+
+        string lower = user.Substring(0, 4).ToLower();
+        int sum = 0;
+
+        for(int i = 0; i < lower.Length; i++)
+        {
+            sum = sum + (int)lower[i];
+        }
+
+        string last = user.Substring(6, 2);
+        string password = "TECH_" + sum.ToString() + last;
+
+        Console.WriteLine("Password: " + password);
+
+    }
+}
