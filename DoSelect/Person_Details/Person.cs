74c061a719bc92cbe65ceed3de87c6cbf5ffec91
Arti Thakur <arti31thakur@gmail.com>
2026-02-04 22:04:12 +0530

    DoSelect Questions

---
+++ DoSelect/Person_Details/Person.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class Person
+{
+    public string Name {get; set;}
+    public string Address {get; set;}
+    public int Age {get; set;}
+}
+
+class PersonImplementation
+{
+    public string GetName(IList<Person> person)
+    {
+        
+    }
+}
