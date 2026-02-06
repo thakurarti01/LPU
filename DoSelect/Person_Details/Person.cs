0d258346c373c1b670445be153196c599c8293b6
Arti Thakur <arti31thakur@gmail.com>
2026-02-06 10:20:28 +0530

    DoSelect Questions

--- DoSelect/Person_Details/Person.cs
+++ DoSelect/Person_Details/Person.cs
@@ -12,6 +12,36 @@ class PersonImplementation
 {
     public string GetName(IList<Person> person)
     {
-        
+        string result = "";
+        foreach(Person p  in person)
+        {
+            result += p.Name + " " + p.Address + " ";
+        }
+        return result;
+    }
+
+    public double Average(IList<Person> person)
+    {
+        // return person.Average(p => p.Age); -- in LINQ
+
+        int sum = 0;
+        foreach(Person p in person)
+        {
+            sum += p.Age;
+        }
+        return (double)sum/person.Count;
+    }
+
+    public int Max(IList<Person> person)
+    {
+        int maxAge = person[0].Age;
+        foreach(Person p in person)
+        {
+            if(p.Age > maxAge)
+            {
+                maxAge = p.Age;
+            } 
+        }
+        return maxAge;
     }
 }
--- DoSelect/Person_Details/Program.cs
+++ DoSelect/Person_Details/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        IList<Person> p = new List<Person>();
+        p.Add(new Person {Name = "Aarya", Address = "A2101", Age = 69});
+        p.Add(new Person {Name = "Daniel", Address = "D104", Age = 40});
+        p.Add(new Person {Name = "Ira", Address = "H801", Age = 25});
+        p.Add(new Person {Name = "Jennifer", Address = "I1704", Age = 33});
+
+        PersonImplementation obj = new PersonImplementation();
+
+        Console.Write(obj.GetName(p));
+        Console.WriteLine();
+        Console.WriteLine(obj.Average(p));
+        Console.WriteLine(obj.Max(p));
+    }
+}
