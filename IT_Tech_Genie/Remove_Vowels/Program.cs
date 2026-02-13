7aa72b1aa79242dbdcf6a5ba75e580496bfc4437
Arti Thakur <arti31thakur@gmail.com>
2026-02-13 14:34:35 +0530

    String Questions

---
+++ IT_Tech_Genie/Remove_Vowels/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your input: ");
+        string input = Console.ReadLine();
+
+        string result = "";
+
+        foreach(char ch in input)
+        {
+            if("aeiouAEIOU".Contains(ch))
+            {
+                continue;   
+            }
+            result += ch;
+        }
+
+        Console.WriteLine("After removing vowels: " + result);
+    }
+}
