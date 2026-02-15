ef7aa3fd9f965ae22f3903d6b122868b0fad1fc2
Arti Thakur <arti31thakur@gmail.com>
2026-02-15 23:50:07 +0530

    String Questions

--- IT_Tech_Genie/Inserting_Between_Characters/Program.cs
+++ IT_Tech_Genie/Inserting_Between_Characters/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        // Console.Write("Enter your input: ");
+        // string input = Console.ReadLine();
+        
+        // string result = "";
+
+        // for(int i = 0; i < input.Length; i++)
+        // {
+        //     result += input[i];
+
+        //     if(i < input.Length - 1)
+        //     {
+        //         result += "*";
+        //     }
+        // }
+        // Console.WriteLine("Your result: " + result);
+
+
+        // -------------------- USING STRINGBUILDER -------------------------
+        Console.Write("Enter your input: ");
+        string input = Console.ReadLine();
+
+        StringBuilder sb = new StringBuilder(input);
+
+        for(int i = 0; i < sb.Length; i += 2)
+        {
+            sb.Insert(i, "*");
+        }
+        Console.WriteLine("Your result: " + sb.ToString());
+    }
+}
