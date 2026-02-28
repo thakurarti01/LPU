b9a04612556ac3a9208c722c954a28dd4800ebcb
Arti Thakur <arti31thakur@gmail.com>
2026-02-28 19:07:15 +0530

    Flight Search Engine

---
+++ Asp.Net/FlightSearchEngine/FlightSearchEngine/Models/FlightResult.cs
@@ -0,0 +1,12 @@
+﻿namespace FlightSearchEngine.Models
+{
+    public class FlightResult
+    {
+		public int FlightId { get; set; }
+		public string FlightName { get; set; }
+		public string FlightType { get; set; }
+		public string FlightSource { get; set; }
+		public string FlightDestination { get; set; }
+		public decimal TotalCost { get; set; }
+	}
+}
---
+++ Asp.Net/FlightSearchEngine/FlightSearchEngine/Models/SearchViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace FlightSearchEngine.Models
+{
+    public class SearchViewModel
+    {
+		[Required]
+		public string Source { get; set; }
+
+		[Required]
+		public string Destination { get; set; }
+
+		[Range(1, 100)]
+		public int NumberOfPersons { get; set; }
+
+		public List<FlightResult> FlightResults { get; set; }
+		public List<FlightHotelResult> FlightHotelResults { get; set; }
+	}
+}
