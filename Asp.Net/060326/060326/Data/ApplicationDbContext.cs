0f39dd39ac97aa5a92f78b8d9ac03a0057dc728a
Arti Thakur <arti31thakur@gmail.com>
2026-03-07 23:55:21 +0530

    Library Management System

--- Asp.Net/060326/060326/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace _060326.Data
-{
-	public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
-	{
-	}
-}
--- Asp.Net/060326/060326/Services/ICalculatorServices.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace _060326.Services
-{
-	public class ICalculatorServices
-	{
-	}
-}
